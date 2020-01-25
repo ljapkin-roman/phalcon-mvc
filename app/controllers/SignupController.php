da71702c5ca785759b0fefdf9fa8a98f83a1b73a
Roma <silvervaza@gmail.com>
2020-01-25 22:03:50 +0200

    add retrun data to form if the data not is valid

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -7,50 +7,46 @@ class SignupController extends Controller
 
     public function indexAction()
     {
-    }
-
-    public function registerAction()
-    {
         $user = new Users();
-        $data = $this->request->getPost();
-        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
-        $user->assign(
-            [
-                "firstname" => $data['firstname'],
-                "lastname" => $data['lastname'],
-                "email" => $data['email'],
-                "password" => $data['password'],
-                'user_type' => $data['status'],
-            ]
-        );
-        $success = $user->save();
+        if ($this->request->getPost())
+        {
+            $dataPOST = $this->request->getPost();
+            $dataPOST['password'] = password_hash($dataPOST['password'], PASSWORD_DEFAULT);
+            $user->assign(
+                $dataPOST
+            );
+            $success = $user->save();
 
-        if ($success) {
-            $this->session->set(
-                'auth',
-                [
-                    'id' =>$user->user_id,
-                    'email' =>$user->email,
-                    'status' => $user->user_type,
-                ]
-            );
-            $this->flashSession->success(
-                'Welcome ' . $user->firstname .'</br>'.
-                "Thank you for registering!"
-            );
-            $this->response->redirect('/');
-        } else {
-            $this->flashSession->success(
-                "Sorry, the following problems were generated: "
-            );
-           $messages = $user->getMessages();
-
-            foreach ($messages as $message) {
-                $this->flashSession->success(
-                    $message->getMessage()
+            if ($success) {
+                $this->session->set(
+                    'auth',
+                    [
+                        'id' =>$user->user_id,
+                        'email' =>$user->email,
+                        'status' => $user->user_type,
+                    ]
                 );
+                $this->flashSession->success(
+                    'Welcome ' . $user->firstname .'</br>'.
+                    "Thank you for registering!"
+                );
+                $this->response->redirect('/');
+            } else {
+                $this->flashSession->error(
+                    "Sorry, the following problems were generated: "
+                );
+               $messages = $user->getMessages();
+
+                foreach ($messages as $message) {
+                    $this->flashSession->success(
+                        $message->getMessage()
+                    );
+                }
+                $dataPOST['password'] = '';
+                $this->view->data = $dataPOST;
             }
-            return true;
         }
     }
+
+
 }
