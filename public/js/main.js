7dc4d7f79457aed720131e8f14a8c8dff727e519
Roma <silvervaza@gmail.com>
2020-01-25 12:50:45 +0200

    complete script that create and full table

--- public/js/main.js
+++ public/js/main.js
@@ -14,8 +14,39 @@ $(".chosen-select").chosen();
 
 let selectUser = document.getElementById("selectUser");
 
+async function createTable(email='')
+{
+    let url = 'http://localhost:80/address/alldata/' + email;
+    url = url.replace(/\s/g, '');
+    let response = await fetch(url);
+    let listAddress = await response.json();
+    let tableBody = document.getElementById('parentBody');
+    tableBody.innerHTML = '';
+    for(let key in listAddress) {
+        addRecord(tableBody, key, listAddress[key]);
+    } 
+}
+createTable();
+function addRecord(table, key, address)
+{
+        let tr = document.createElement('tr')
+        let innerHtml = 
+            "<th>" +  address['city'] + "</th>" +
+            "<th>" +  address['region'] + "</th>" +
+            "<th>" +  address['street'] + "</th>" +
+            "<th>" +  address['postcode'] + "</th>" +
+            "<th>" +  address['owner']  + "</th>";
+        tr.innerHTML = innerHtml;
+        table.append(tr);
+}
+
+
 function getAddressUser(user)
 {
     let email = user.options[user.selectedIndex].innerHTML;
-    return email;
+    if (email == 'all') {
+        email = '';
+    }
+    createTable(email);
 }
+
